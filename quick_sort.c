683156f523cab2c93731747c70953b8808cef94a
canon4444 <haruharu724@hotmail.com>
2015-01-01 16:03:17 +0900

    Create quick_sort.c

---
+++ quick_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define NUM 100
+
+int src[NUM];
+
+void quick_sort()
+{
+
+
+
+}
+
+int main()
+{
+    int i;
+    
+    srand(time(0));
+    
+    for( i = 0; i < NUM; ++i ) src[i] = rand() % NUM;
+    
+    quick_sort();
+    
+    for( i = 0; i < NUM; ++i ){ printf("%d", src[i]); if( i & !(i%6) ) printf("\n"); }
+    
+    return 0;
+}
