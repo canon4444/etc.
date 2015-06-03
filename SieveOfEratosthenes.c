83033609e926579db71e99bf8b70ae76c391abc6
canon4444 <canon4444@users.noreply.github.com>
2015-06-04 04:58:31 +0900

    Update SieveOfEratosthenes.c

--- SieveOfEratosthenes.c
+++ SieveOfEratosthenes.c
@@ -1,35 +1,44 @@
 #include <stdio.h>
+#include <math.h>
 
-int prime[1000];
+#define X 120
+
+int num[X], prime[X];
 int value, cnt;
 
-void init()
-{
-    value = 2;
-    cnt = 0;
-}
 
 int main()
 {
-    int i, flag;
+    int i, j, rootX = sqrt(X)+1;
 
-    init();
+    cnt = 0;
 
-    while( cnt < 1000 ){
-        flag = 1;
-
-        for( i = 0; i < cnt; ++i ){
-            if( value % prime[i] == 0 ){
-                flag = 0;
-                break;
-            }
-        }
-        if( flag ) prime[cnt++] = value;
-
-        ++value;
+        for( i = 0; i < X; ++i ){ //初期化
+        if( i == 0 || i == 1 ) num[i] = -1;
+        else num[i] = i;
     }
 
-    for( i = 0; i < cnt; ++i ){
+    for( i = 0; i < X; ++i ){ //初期化
+        prime[i] = -1;
+    }
+    
+
+    for( i = 0; i < rootX; ++i, ++cnt ){
+        while( num[i] == -1 ) ++i; //篩い落とした値を飛ばす
+        value = prime[cnt] = num[i]; //基数を素数表へ移す
+        num[i] = -1;
+
+        for( j = 0; value * j < X; ++j ){ //倍数を篩い落とす
+            num[value * j] = -1;
+        }
+    }
+
+    for( i = 0; i < X; ++i ){ //残りの値を素数表へ移す
+        if( num[i] != -1 ) prime[cnt++] = num[i];
+    }
+    
+
+    for( i = 0; i < cnt; ++i ){ //出力
         printf("%d", prime[i]);
         if( (i+1) % 5 == 0 ) printf("\n");
         else printf(" ");
